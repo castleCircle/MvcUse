ccc992d4d571fcc01d03d97493d0c4768869de31
wony <yoonemong@naver.com>
2019-06-26 07:24:18 +0900

    power up:

--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -2,21 +2,50 @@ package com.example.demo;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE )
 public class SampleController {
 
 	//GetMapping
 	//@RequestMapping(value = "/hello" , method = {RequestMethod.GET,RequestMethod.PUT})
 	//@RequestMapping({"/hello","/hi"})
 	
-	@RequestMapping(value = "/hello" , consumes = MediaType.APPLICATION_JSON_UTF8_VALUE ,produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/helloyo" , consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public String helloYo() {
+		return "hello sungwon";
+	}
+	
+	@GetHelloMapping
 	@ResponseBody
 	public String hello() {
-		return "hello sungwon";
+		return "hello";
+	}
+	
+	//
+	@GetMapping("/events")
+	@ResponseBody
+	public String events() {
+		return "events";
+	}
+	
+	@GetMapping("/events/{id}")
+	@ResponseBody
+	public String events1(@PathVariable("id") int id) {
+		return "event";
+	}
+	
+	@PostMapping(value="/events" ,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public String createEvent() {
+		return "event";
 	}
 	
 	
--- src/test/java/com/example/demo/SampleControllerTest.java
+++ src/test/java/com/example/demo/SampleControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,12 +21,41 @@ public class SampleControllerTest {
 	MockMvc mockMvc;
 	
 	@Test
-	public void helloTest() throws Exception {
+	public void helloYoTest() throws Exception {
 		
-		mockMvc.perform(get("/hello")
+		mockMvc.perform(get("/helloyo")
 				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
 				.andDo(print())
 				.andExpect(status().isOk());
 		
 	}
+	
+	@Test
+	public void helloTest() throws Exception{
+		mockMvc.perform(get("/hello"))
+			.andDo(print())
+			.andExpect(status().isOk());
+		
+	}
+	
+	
+	@Test
+	public void getEvents() throws Exception{
+		mockMvc.perform(get("/events"))
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void getEventsWithId() throws Exception{
+		mockMvc.perform(get("/events/1")).andExpect(status().isOk());
+		mockMvc.perform(get("/events/2")).andExpect(status().isOk());
+		mockMvc.perform(get("/events/3")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void createEvent() throws Exception{
+		mockMvc.perform(post("/events").contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk());
+	}
+	 
 }
